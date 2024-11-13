34452bada93fcb0a8d96834a9233543a1ed8df45
smit7103 <92993003+smit7103@users.noreply.github.com>
2024-11-13 14:15:19 -0500

    Delete src/ca/sheridancollege/project directory

--- src/ca/sheridancollege/project/PlayingCard.java
+++
@@ -1,7 +0,0 @@
-package ca.sheridancollege.project;
-
-public class PlayingCard extends Card {
-    public PlayingCard(String suit, String rank, int value) {
-        super(suit, rank, value);
-    }
-}
